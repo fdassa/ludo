e8a33f5eb7ac6db8030e5c53770cdadb7c675d63
fdassa <fdassa1@gmail.com>
2018-11-12 15:32:55 -0200

    extrai logica de centralizacao na tela para um metodo

--- src/InterfaceDoJogo.java
+++ src/InterfaceDoJogo.java
@@ -8,19 +8,17 @@ public class InterfaceDoJogo extends JFrame {
 	public InterfaceDoJogo() {
 		this.setSize(768, 640);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Adiciona tabuleiro
 		this.add(new Tabuleiro());
-
-		// Centraliza na tela do computador
+		this.setVisible(true);
+		centralizaNaTela();
+	}
+	
+	private void centralizaNaTela() {
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension dim = tk.getScreenSize();
 		final int xPos = (dim.width / 2) - (this.getWidth() / 2);
 		final int yPos = (dim.height / 2) - (this.getHeight() / 2);
 		this.setLocation(xPos, yPos);
 		this.setResizable(false);
-
-		// Exibe frame
-		this.setVisible(true);
 	}
 }
